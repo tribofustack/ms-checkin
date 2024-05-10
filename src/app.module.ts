b384616d1539ef0c88f95405ac9743418d3c0554
Pedro Quessada <pedro.rafael.quessada@gmail.com>
2024-05-10 16:04:02 -0300

    delete invalid files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { OrderModule } from './external/adapters/checkout/order.module';
 import { CustomerModule } from './external/adapters/customer/customer.module';
 import { ProductModule } from './external/adapters/product/product.module';
-import { PaymentModule } from './external/adapters/payment/payment.module';
 
 import DatabaseModule from './external/infra/database';
 import QueueModule from './external/infra/queue';
@@ -12,10 +10,8 @@ import TokenGeneratorModule from './external/infra/tokens';
 import { Jwt } from './external/infra/tokens/jwt/jwt';
 @Module({
   imports: [
-    OrderModule,
     CustomerModule,
     ProductModule,
-    PaymentModule,
     QueueModule,
     DatabaseModule,
     TokenGeneratorModule,
