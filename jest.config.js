69f85e9f40ee2e9e24eda263b61c84f7d3d774d7
deal-machine <95deal@gmail.com>
2024-05-17 14:40:47 -0300

    feat: config jest for ignore invalid locals

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,39 @@
-module.exports = {
-    preset: 'ts-jest',
+const ignoreFiles = [
+    "/node_modules/",
+    "jest.config.js",
+    "main.ts",
+    "/external/",
+    "app.module.ts",
+    "/internal/application/configs/swagger.config.ts",
+    "/internal/application/docs/",
+    "/internal/application/ports/",
+]
+
+module.exports = {    
     testEnvironment: 'node',
-    transform: {
-        '^.+\\.(t|j)s$': 'ts-jest',
-    },
-    moduleFileExtensions: [
-        "js",
-        "json",
-        "ts"
-      ],
-    rootDir: "src",
-    testRegex: ".*\\.spec\\.ts$",    
-    collectCoverageFrom: [
-    "**/*.(t|j)s"
-    ],
-    coverageDirectory: "./coverage",
+    preset: 'ts-jest',
+    roots: ["<rootDir>"],
+    clearMocks: true,
+    collectCoverageFrom: ["**/*.ts"],
     "moduleNameMapper": {
-        "^src/(.*)$": "<rootDir>/$1"
-      }
-}
+        "src/(.*)": "<rootDir>/$1"
+    },
+    moduleDirectories: [
+        "node_modules",
+        "src"
+    ],
+    modulePaths: [
+        "<rootDir>/src/"
+    ],
+    coverageProvider: "v8",
+    rootDir: "./src",
+    testMatch: [
+        "**/__tests__/**/*.[jt]s?(x)",
+        "**/?(*.)+(spec|test).[tj]s?(x)"
+    ],
+    // testPathIgnorePatterns: ignoreFiles,
+    coveragePathIgnorePatterns: ignoreFiles,
+    transform: {
+        "^.+\\.(t|j)sx?$": "ts-jest",
+    },
+};
